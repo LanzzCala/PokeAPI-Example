d03d7628c913b7a878fdd4fdb56a18de615922f1
Lanzz Cala <lanzzcala@gmail.com>
2026-01-25 19:33:53 -0800

    Confirm response is OK. Import response using .json method to python dictionary

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ base_url = "https://pokeapi.co/api/v2/"
 def get_pokemon_info(name):
     url=f"{base_url}/pokemon/{pokemon_name}"
     response = requests.get(url)
-    print(response)
+    #print(response)
 
-pokemon_name = "pikachu"
+    if response.status_code == 200:
+        print("Data retrieved!")
+        pokemon_data = response.json()
+        print(pokemon_data)
+    else:
+        print(f"Failed to retireve data {response.status_code}")
+
+pokemon_name = "machoke"
 get_pokemon_info(pokemon_name)
