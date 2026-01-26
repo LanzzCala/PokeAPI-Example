37ce1a3d3ccfcde5e4754b051a932a45428c930e
Lanzz Cala <lanzzcala@gmail.com>
2026-01-25 19:22:39 -0800

    Establish function to get Pokemon info. Establish base URL and receive a response

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import requests
 
-main_url = "https://pokeapi.co/api/v2/"
+base_url = "https://pokeapi.co/api/v2/"
+
+def get_pokemon_info(name):
+    url=f"{base_url}/pokemon/{pokemon_name}"
+    response = requests.get(url)
+    
+pokemon_name = "pikachu"
+get_pokemon_info(pokemon_name)
